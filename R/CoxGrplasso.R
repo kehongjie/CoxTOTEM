af0c44d21157b50bc81f30196215c888da310a0a
Hongjie Ke <61289103+kehongjie@users.noreply.github.com>
2020-04-12 23:44:31 -0400

    Update CoxGrplasso.R

--- R/CoxGrplasso.R
+++ R/CoxGrplasso.R
@@ -1,27 +1,27 @@
 ##' Fit a multiple-study Cox model with group lasso regularization.
-##' (or?) Group lasso for Cox model with multiple studies.
 ##' 
-##' This function adopts a group lasso penalty in the partial log-likelihood
+##' This function adopts a group lasso penalty to the partial log-likelihood
 ##' for Cox model with multiple studies. It is used after the screening by 
 ##' function \code{CoxSIS} to select the final set of features with
 ##' nonzero coefficients. The optimization problem is solved by an ADMM
 ##' algorithm. It is the second stage (i.e. regularization stage) of 
-##' Cox-TOTEM algorithm.
+##' Cox-TOTEM method.
 ##' 
-##' @param data.list: A list in which every element represents a study. 
-##' Within each element (study), \code{time} is the follow up time (n * 1), 
-##' \code{event} is the status indicator (n * 1) with 0=alive and 1=dead, and
-##' \code{X} is the covariates matrix of dimensions n * p.
-##' @param index: The indices of potential nonzero coefficients selected by
-##' screening stage. Typically the "index" output form the function \code{CoxSIS}.  
-##' @param lambda: The tuning parameter for regularization. It should be a 
-##' single value. 
-##' @param rho: TBD.(?) 
+##' @param data.list: A list in which every element represents a study, 
+##' typically after screening. Within each element (study), \code{time} is 
+##' the follow up time (\eqn{n \times 1}), \code{event} is the status indicator 
+##' (\eqn{n \times 1}) with 0=alive and 1=dead, and \code{X} is the covariates 
+##' matrix of dimensions \eqn{n \times p}, where p is the number of covariates 
+##' left after screening.
+##' @param lambda: The penalty parameter in regularization. It is a single 
+##' optimal value that needs to be tuned in \code{cv.CoxGrplasso}. 
+##' @param rho: The step size in ADMM algorithm. Default is 50, and change of 
+##' \code{rho} is typically not recommended.
 ##' @param iter: Maximum iterations. Default is 100.
 ##' @param tol: Convergence threshold for ADMM algorithm. Default is \code{1E-4}.
 
-##' @return A matrix of estimated coefficients, in which each row is a predictor
-##' selected by the screening stage and each column is a study.
+##' @return A matrix of estimated coefficients, in which each row is a covariate
+##' and each column is a study.
 ##' @export
 ##' @examples
 ##' \dontrun{
@@ -32,7 +32,7 @@
 ##' n <- 50 ## sample size
 ##' p <- 200 ## number of covariates
 ##' rho <- 0.5
-##' K <- 5 ## number of studies
+##' S <- 5 ## number of studies
 ##' ss <- 2 ## number of signal/true predictors
 ##' lambda0 <- 1 ## baseline hazard
 ##' rate <- 0.2 ## parameter for Exponential distribution
@@ -41,15 +41,15 @@
 ##' ## set up the coefficients
 ##' true.ind <- sample(1:p,size=ss) ## signal index
 ##' noise.ind <- (1:p)[-true.ind]
-##' beta.mat <- matrix(0,nrow=p,ncol=K)
+##' beta.mat <- matrix(0,nrow=p,ncol=S)
 ##' for(jj in 1:length(true.ind)){
 ##'   beta.mat[true.ind[jj],] <- mu
 ##' }
 ##' 
 ##' ## simulate data for each study
-##' data.list <- vector("list",K)
-##' for(k in 1:K){
-##'   beta <- beta.mat[,k]
+##' data.list <- vector("list",S)
+##' for(s in 1:S){
+##'   beta <- beta.mat[,s]
 ##'   sigma <- toeplitz(rho^c(0,1:(p-1)))
 ##'   X <- mvrnorm(n, rep(0,p), sigma)
 ##'   U <- runif(n,0,1)
@@ -57,44 +57,54 @@
 ##'   time <- -log(U) / (lambda0*exp(X%*%beta)) 
 ##'   Y <- pmin(time,C)
 ##'   D <- ifelse(time<C,1,0)
-##'   data.list[[k]] <- list(time=Y,event=D,X=X)
+##'   data.list[[s]] <- list(time=Y,event=D,X=X)
 ##' }
 ##' 
 ##' ## screening
 ##' res.CoxSIS <- CoxSIS(data.list=data.list, alpha1=1e-4, alpha2=0.05)
-##' res.CoxSIS$index
+##' 
+##' ## remove variables with zero coefficients
+##' index.sis <- res.CoxSIS$index
+##' data.list.sis <- NULL
+##' for(s in 1:S){
+##'  dat <- data.list[[s]]
+##'  X <- dat[["X"]]
+##'  X <- X[,index.sis]
+##'  dat[["X"]] <- X
+##'  data.list.sis[[s]] <- dat
+##' }
 ##' 
 ##' ## group lasso
-##' sis.ind <- res.CoxSIS$index
-##' coef.final <- CoxGrplasso(data.list,sis.ind,lambda=30,rho=50,iter=100,tol=1e-4)
+##' coef.final <- CoxGrplasso(data.list.sis,lambda=30,rho=50,iter=100,tol=1e-4)
 ##' head(coef.final)
 ##' }
 
 
-CoxGrplasso <- function(data.list, index, lambda, rho=50, iter=100, tol=1e-4) {
-  p <- length(index)
-  K <- length(data.list)    
+CoxGrplasso <- function(data.list, lambda, rho=50, iter=100, tol=1e-4) {
+  S <- length(data.list)    
+  p <- ncol(data.list[[1]][["X"]])
+
   dat.list <- NULL
-  for(k in 1:K){
-    dat <- data.list[[k]]
+  for(s in 1:S){
+    dat <- data.list[[s]]
     X <- dat[["X"]]
-    X <- X[,index]
     time <- c(dat[["time"]])
     event <- c(dat[["event"]])
     ind <- order(time)        
     datk <- list(time=time[ind],event=event[ind],x=X[ind,])
-    dat.list[[k]] <- datk
+    dat.list[[s]] <- datk
   }
+  
   ## initialization
-  Theta <- matrix(0,nrow=p,ncol=K)
-  z.mat <- matrix(0,nrow=p,ncol=K)
-  u.mat <- matrix(0,nrow=p,ncol=K)
+  Theta <- matrix(0,nrow=p,ncol=S)
+  z.mat <- matrix(0,nrow=p,ncol=S)
+  u.mat <- matrix(0,nrow=p,ncol=S)
   ## iterations
   for(i in 1:iter){
     ## update on Theta
-    for(k in 1:K){
-      Theta[,k] <- updateTheta(dat=dat.list[[k]],theta=Theta[,k],z=z.mat[,k],
-                               u=u.mat[,k],rho=rho,tol=tol)
+    for(s in 1:S){
+      Theta[,s] <- updateTheta(dat=dat.list[[s]],theta=Theta[,s],z=z.mat[,s],
+                               u=u.mat[,s],rho=rho,tol=tol)
     }
     ## update on Z
     z.mat.old <- z.mat
